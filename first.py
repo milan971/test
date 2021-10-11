6b4363794a69cf9b51d946deaa958969bf23b138
Pandya Milan Nileshkumar <milan.pandya@iitb.ac.in>
2021-10-11 15:14:22 +0530

    added hungry prompt

--- first.py
+++ first.py
@@ -5,4 +5,11 @@ Created on Mon Oct 11 14:55:01 2021
 @author: Milan
 """
 
-print("i am hungry")
+#print("i am hungry")
+
+hungry = input("are you hungry?")
+
+if hungry == "yes":
+    print("eat food")
+else:
+    print("Do work")
